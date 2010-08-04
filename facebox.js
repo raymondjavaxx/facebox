74aa26c63e075fe47e11265ca4c14fc8ec12838d
Ramon <raymondjavaxx@gmail.com>
2010-08-04 19:50:32 -0400

    use CSS3 rgba for border

--- facebox.js
+++ facebox.js
@@ -91,29 +91,15 @@
       faceboxHtml  : '\
     <div id="facebox" style="display:none;"> \
       <div class="popup"> \
-        <table> \
-          <tbody id="facebox-tbody"> \
-            <tr> \
-              <td class="tl"/><td class="b"/><td class="tr"/> \
-            </tr> \
-            <tr> \
-              <td class="b"/> \
-              <td class="body"> \
-                <div class="content"> \
-                </div> \
-                <div class="footer"> \
-                  <a href="#" class="close"> \
-                    <img src="/facebox/closelabel.gif" title="close" class="close_image" /> \
-                  </a> \
-                </div> \
-              </td> \
-              <td class="b"/> \
-            </tr> \
-            <tr> \
-              <td class="bl"/><td class="b"/><td class="br"/> \
-            </tr> \
-          </tbody> \
-        </table> \
+          <div class="body"> \
+            <div class="content"> \
+            </div> \
+            <div class="footer"> \
+              <a href="#" class="close"> \
+                <img src="/facebox/closelabel.gif" title="close" class="close_image" /> \
+              </a> \
+            </div> \
+          </div> \
       </div> \
     </div>'
     },
@@ -145,7 +131,7 @@
       $('#facebox .content').append(data)
       $('#facebox .loading').remove()
       $('#facebox .body').children().fadeIn('normal')
-      $('#facebox').css('left', $(window).width() / 2 - ($('#facebox table').width() / 2))
+      $('#facebox').css('left', $(window).width() / 2 - ($('#facebox div.body').width() / 2))
       $(document).trigger('reveal.facebox').trigger('afterReveal.facebox')
     },
 
